a969f409ea10e5377f7f7ef6eddc76e1860a857b
zlawson13 <zach@executivedsllc.com>
2019-09-10 15:04:04 -0500

    updating connection details

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-flat-10155',
-    user : 'postgre',
-    password : '',
-    database : 'smartbrain'
+    host : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 

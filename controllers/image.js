be82a33cd22dbd7592d70eadd2954292025186ed
zlawson13 <zach@executivedsllc.com>
2019-09-10 10:45:39 -0500

    Structuring code

---
+++ controllers/image.js
@@ -0,0 +1,29 @@
+const Clarifai = require('./node_modules/clarifai');
+const app = new Clarifai.App({
+ apiKey: 'd3dce6d0252a49458eff5f953616b3ac'
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db('users').where('id', '=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+    res.json(entries[0]);
+  })
+  .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+  handleImage,
+  handleApiCall
+}

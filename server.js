fa36aaf872f82a29eab783d9d8488ca693084bcf
zlawson13 <zach@executivedsllc.com>
2019-09-11 11:14:46 -0500

    Pushing Patches

--- server.js
+++ server.js
@@ -1,35 +1,36 @@
 const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
-const cors = require('cors');
+const bcrypt = require('bcrypt-nodejs');
+const core = require('cors');
 const knex = require('knex');
-
 const register = require('./controllers/register');
-const signin = require('./controllers/sigin');
+const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
-const db = knex({
-  client: 'pg',
-  connection: {
-    connectionString : process.env.DATABASE_URL,
-    ssl: true,
-  }
-});
-
-const app = express();
-
-app.use(cors())
+app.use(core());
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> { res.send('It Is Working') })
-app.post('/signin', signin.handleSignin(db, bcrypt))
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+const db = knex({
+	client:'pg',
+	connection: {
+		connectionString: process.env.DATABASE_URL,
+		ssl: true,
+	}
+});
 
+app.get('/', (req, res)=> {res.send(db.users)})
+app.post('/signin', (req, res) => {signin.handleSignIn(req, res, db, bcrypt)})
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
+app.get('/profile/:id', (req, res) => {profile.handleProfile(req, res, db)})
+app.put('/image', (req, res) => {image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
 
-app.listen(3000, () => {
-	console.log(`listening on port 3000`)
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3001;
+}
+app.listen(port, () => {
+	console.log(`listening on port ${port}`)
 });
